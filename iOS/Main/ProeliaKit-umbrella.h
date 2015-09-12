ae44d3ad7acff03e09272fb3acbd1d2cd42b65e2
Paul Schifferer <paul@schifferers.net>
2015-09-12 15:04:13 -0700

    Specify a module map.

---
+++ iOS/Main/ProeliaKit-umbrella.h
@@ -0,0 +1,51 @@
+//
+//  ProeliaKit-umbrella.h
+//  ProeliaKit
+//
+//  Created by Paul Schifferer on 3/1/15.
+//  Copyright (c) 2015 Pilgrimage Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for ProeliaKit.
+FOUNDATION_EXPORT double ProeliaKitVersionNumber;
+
+//! Project version string for ProeliaKit.
+FOUNDATION_EXPORT const unsigned char ProeliaKitVersionString[];
+
+// -- Constants --
+
+#import "EncounterConstants.h"
+
+// -- Protocols --
+
+#import "ConditionProvider.h"
+
+// -- Value objects --
+
+#import "Auditable.h"
+#import "BaseIdObject.h"
+#import "AbstractEncounter.h"
+#import "AbstractMap.h"
+#import "AbstractMapLocation.h"
+#import "AbstractMapTile.h"
+#import "AbstractParticipant.h"
+#import "AbstractRegion.h"
+#import "Campaign.h"
+#import "EncounterGroup.h"
+#import "Encounter.h"
+#import "EncounterTimelineEntry.h"
+#import "EncounterMap.h"
+#import "EncounterMapLocation.h"
+#import "EncounterMapTile.h"
+#import "EncounterParticipant.h"
+#import "EncounterRegion.h"
+#import "Condition.h"
+#import "EncounterTemplate.h"
+#import "EncounterMapTemplate.h"
+#import "EncounterMapLocationTemplate.h"
+#import "EncounterMapTileTemplate.h"
+#import "EncounterParticipantTemplate.h"
+#import "EncounterRegionTemplate.h"
