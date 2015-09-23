b22740e0242a47b7b4deb9f01be6b9a025aa9e60
Paul Schifferer <paul@schifferers.net>
2015-09-22 17:01:17 -0700

    Updated wording.

--- Shared/EffectConstants.h
+++ Shared/EffectConstants.h
@@ -1,5 +1,5 @@
 //
-//  ConditionConstants.h
+//  EffectConstants.h
 //  ProeliaKit
 //
 //  Created by Paul Schifferer on 9/21/15.
@@ -10,61 +10,62 @@
 
 
 /**
+ * The standard point size for Effect images.
  */
 extern float const EffectImageSize;
 
 /**
- * The type of condition.
+ * The type of effect.
  */
 typedef NS_ENUM(NSInteger, EffectType) {
     /**
-     * A condition that is of type 'once' is applied one time, and then automatically removed from the thing to which
+     * An effect that is of type 'once' is applied one time, and then automatically removed from the thing to which
      * it is attached.
      */
     EffectTypeOnce,
     /**
-     * A condition of type 'ongoing' is applied each time (by round, turn, etc.) until the end type is satisfied.
+     * An effect of type 'ongoing' is applied each time (by round, turn, etc.) until the end type is satisfied.
      */
     EffectTypeOngoing,
     /**
-     * A 'custom' condition is maintained until the game master removes it manually.
+     * A 'custom' effect is maintained until the game master removes it manually.
      */
     EffectTypeCustom,
 };
 
 /**
- * The requirement that must be satisfied in order for the condition to be removed by the encounter manager.
+ * The requirement that must be satisfied in order for the effect to be removed by the encounter manager.
  */
 typedef NS_ENUM(NSInteger, EffectEnd) {
     /**
      * A saving throw (handled outside of the encounter manager, which success is specified by the user) must
-     * succeed in order to remove this condition. The encounter manager will issue a reminder at the appropriate time
+     * succeed in order to remove this effect. The encounter manager will issue a reminder at the appropriate time
      * for this requirement.
      */
     EffectEndSave,
     /**
-     * The condition will end at the start of the specified participant's turn. The participant's name or ID is specified
-     * in the 'endValue' property. If no value is specified there, the participant to whom the condition is attached is
+     * The effect will end at the start of the specified participant's turn. The participant's name or ID is specified
+     * in the 'endValue' property. If no value is specified there, the participant to whom the effect is attached is
      * assumed.
      */
     EffectEndStartOfTurn,
     /**
-     * The condition will end at the end of the specified participant's turn. The participant's name or ID is specified
-     * in the 'endValue' property. If no value is specified there, the participant to whom the condition is attached is
+     * The effect will end at the end of the specified participant's turn. The participant's name or ID is specified
+     * in the 'endValue' property. If no value is specified there, the participant to whom the effect is attached is
      * assumed.
      */
     EffectEndEndOfTurn,
     /**
-     * The condition will last until the encounter is ended, or it is manually removed.
+     * The effect will last until the encounter is ended, or it is manually removed.
      */
     EffectEndEndOfEncounter,
     /**
-     * The condition is applied by a region, and affects any participants within the region's bounds. The condition only
+     * The effect is applied by a region, and affects any participants within the region's bounds. The effect only
      * ends when the participant moves outside the region's bounds.
      */
     EffectEndRegion,
     /**
-     * The condition last for a fixed number of rounds or turns, as specified in the 'endValue' property. The format of
+     * The effect last for a fixed number of rounds or turns, as specified in the 'endValue' property. The format of
      * the value is "#r" for a number of rounds, "#t" for a number of turns. A numeric value without a suffix is assumed
      * to be 'rounds'.
      */
@@ -72,48 +73,48 @@ typedef NS_ENUM(NSInteger, EffectEnd) {
 };
 
 /**
- * The source of the condition.
+ * The source of the effect.
  */
 typedef NS_ENUM(NSInteger, EffectSource) {
     /**
-     * The condition is attached to a participant, and affects said participant.
+     * The effect is attached to a participant, and affects said participant.
      */
     EffectSourceSelf,
     /**
-     * The condition is attached to a region, and affects some participants in the region's bounds (see EffectTargets
+     * The effect is attached to a region, and affects some participants in the region's bounds (see EffectTargets
      * for details).
      */
     EffectSourceRegion,
     /**
-     * The condition is applied by the game system.
+     * The effect is applied by the game system.
      */
     EffectSourceGameSystem,
 };
 
 /**
- * A set of options indicating what types of things are affected by the condition. This is useful for when a condition
+ * A set of options indicating what types of things are targeted by the effect. This is useful for when an effect
  * is attached to a region, and a mix of participants are within the region's bounds, allowing the encounter manager to
- * intelligently determine who should be affected by the condition.
+ * intelligently determine who should be targeted by the effect.
  */
 typedef NS_OPTIONS(NSInteger, EffectTargets) {
     /**
-     * The condition affects no one.
+     * The effect targets no one.
      */
     EffectTargetsNone = 0,
     /**
-     * The condition affects only participants of type PC.
+     * The effect targets only participants of type PC.
      */
     EffectTargetsPC = 1 << 1,
     /**
-     * The condition affects only participants of type Adversary.
+     * The effect targets only participants of type Adversary.
      */
     EffectTargetsAdversary = 1 << 2,
     /**
-     * The condition affects only participants of type Neutral.
+     * The effect targets only participants of type Neutral.
      */
     EffectTargetsNeutral = 1 << 3,
     /**
-     * The condition affects only participants of type Object.
+     * The effect targets only participants of type Object.
      */
     EffectTargetsObject = 1 << 4,
     /**
